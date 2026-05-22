a72684369eda825bc43462f418d1b7e484b73cc5
Леонид Дьяков <amogusr1k@gmail.com>
2026-05-22 15:28:27 +0300

    test: fix include order in BehaviorPresets test skeleton

--- tests/scripting/test_BehaviorPresets.cpp
+++ tests/scripting/test_BehaviorPresets.cpp
@@ -1,9 +1,8 @@
-#include <gtest/gtest.h>
-#include <nlohmann/json.hpp>
-
 #include "core/GameObject.hpp"
 #include "scripting/LuaScript.hpp"
 #include "scripting/LuaScriptEngine.hpp"
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
 
 using dice::core::GameObject;
 using dice::scripting::LuaScriptEngine;
