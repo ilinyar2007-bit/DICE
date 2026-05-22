26d36c7dc1149bfd6d7f960a63d76d9a0a6f9729
Леонид Дьяков <amogusr1k@gmail.com>
2026-05-22 15:22:34 +0300

    test: add behavior presets test infrastructure

---
+++ tests/scripting/test_BehaviorPresets.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+
+#include "core/GameObject.hpp"
+#include "scripting/LuaScript.hpp"
+#include "scripting/LuaScriptEngine.hpp"
+
+using dice::core::GameObject;
+using dice::scripting::LuaScriptEngine;
+using dice::scripting::kEventOnClick;
